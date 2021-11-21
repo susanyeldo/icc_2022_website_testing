15de0df7ecf61058b6ffbff4b86af758c3aed78c
Vaibhav Jalani <80616034+vaibhavjalani@users.noreply.github.com>
2021-11-22 01:59:04 +0530

    Add files via upload

---
+++ js/background.cycle-custom.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+
+var fadeShow = $(".background").fadeShow({
+
+
+
+	correctRatio: true,
+
+
+
+	shuffle: true,
+
+
+
+	speed: 2500,
+
+
+
+	images: ['images/bg-1.jpg',
+
+
+
+			 'images/bg-2.jpg',
+			 'images/bg-3.jpg']
+
+
+
+
+
+
+
+});
