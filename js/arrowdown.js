ae2a2ee032dc8b82498cdcd5036cb34cd292d20d
vaibhavjalani <80616034+vaibhavjalani@users.noreply.github.com>
2021-11-29 13:21:42 +0530

    Updated code

---
+++ js/arrowdown.js
@@ -0,0 +1,14 @@
+let arrowdown = document.getElementById("arrowdown")
+let arrowdown2 = document.getElementById("arrowdown2")
+
+window.addEventListener('scroll',() => {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        arrowdown.style.display = "none";
+        arrowdown2.style.display = "none";
+    }
+    else
+    {
+        arrowdown.style.display = "block";
+        arrowdown2.style.display = "block";
+    }
+})
---
+++ js/registerbtn.js
@@ -0,0 +1,55 @@
+let home = document.getElementById("home");
+let register = document.getElementById("menu2")
+let home1 = document.getElementById("home1");
+let menu1 = document.getElementById("men1")
+let menu2 = document.getElementById("men2");
+let regbtn = document.getElementById("reghover")
+
+let ahome = document.getElementById("aHome");
+let amenu2 = document.getElementById("aMenu2");
+let amenu1 = document.getElementById("aMenu1");
+
+let check1 = ahome.getAttribute("aria-expanded");
+let check2 = amenu2.getAttribute("aria-expanded");
+let check3 = amenu1.getAttribute("aria-expanded");
+
+function hiden()
+{
+    regbtn.style.display="none";
+}
+function unhide()
+{
+    regbtn.style.display="block";
+}
+
+function change()
+{
+    home.classList.remove("active","in")
+    register.classList.add("active","in")
+    home1.classList.remove("active")
+    menu1.classList.remove("active")
+    menu2.classList.add("active")
+
+
+    if(check1=="true" && check2=="false")
+    {
+        check1 = "false";
+        check2 = "true";
+    }
+    else{
+        check1 = "true";
+        check2 = "false";
+    }
+
+    if(check3=="true" && check2=="false")
+    {
+        check3 = "false";
+        check2 = "true";
+    }
+    else{
+        check3 = "true";
+        check2 = "false";
+    }
+
+}
+
